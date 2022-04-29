dc4b8ca4ba93715a4207febbcc413b4e3b83650c
NashiDetki <93508687+NashiDetki@users.noreply.github.com>
2022-04-29 10:57:22 +0500

    запрос по com 8 каналов Adam-4017

--- index.js
+++ index.js
@@ -7,21 +7,73 @@ buttonWrite.textContent = "Write";
 buttonRead.textContent = "Read";
 buttonOff.textContent = "Disconect";
 
+var port,
+  textEncoder,
+  writableStreamClosed,
+  writer,
+  historyIndex = -1;
+
 buttonOn.addEventListener("click", async () => {
-
-  const port = await navigator.serial.requestPort();
-  await port.open({ baudRate: 9600 });
- 
-
+  connectSerial()
 });
 
 buttonWrite.addEventListener("click", async () => {
+  sendSerialLine()
 });
 
 buttonRead.addEventListener("click", async () => {
-
+  // listenToPort()
 });
 
-buttonOff.addEventListener("click", async () => {
+buttonOff.addEventListener("click", async () => {});
 
-});
+setInterval(sendSerialLine, 100);
+
+let count;
+
+async function sendSerialLine() {
+ 
+  if (count<7){
+    count=count + 1; 
+  } else {
+    count=0;
+  }
+  console.log(count);
+  console.log(`#01${count}\r\n`);
+  await writer.write(`#01${count}\r\n`);
+  
+}
+
+async function listenToPort() {
+  const textDecoder = new TextDecoderStream();
+  const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+  const reader = textDecoder.readable.getReader();
+
+  // Listen to data coming from the serial device.
+  while (true) {
+    const { value, done } = await reader.read();
+    if (done) {
+      // Allow the serial port to be closed later.
+      //reader.releaseLock();
+      break;
+    }
+    // value is a string.
+    console.log(value);
+  }
+}
+
+async function connectSerial() {
+  try {
+    // Prompt user to select any serial port.
+    port = await navigator.serial.requestPort();
+    await port.open({ baudRate: 9600 });
+    listenToPort();
+
+    textEncoder = new TextEncoderStream();
+    writableStreamClosed = textEncoder.readable.pipeTo(port.writable);
+
+    writer = textEncoder.writable.getWriter();
+  } catch {
+    alert("Serial Connection Failed");
+  }
+}
